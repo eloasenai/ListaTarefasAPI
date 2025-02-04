db1369cfe968f10c6db47eaf99c7360c25d6edf2
eloa <francodosreissilvae@gmail.com>
2025-02-04 14:43:41 -0300

    Adicionar arquivos de projeto.

---
+++ ListaTarefasAPI/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace ListaTarefasAPI.Models
+{
+    public class Tarefa
+    {
+        public int Tarefaid { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public int Tipotarefaid { get; set; }
+        public Tipotarefa? Tipotarefa { get; set; }
+    }
+}
---
+++ ListaTarefasAPI/Models/Tipotarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaTarefasAPI.Models
+{
+    public class Tipotarefa
+    {
+        public int id { get; set; }
+        public string tipo { get; set; }
+
+
+    }
+}
